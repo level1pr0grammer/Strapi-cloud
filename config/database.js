671a329c1a63f8f06ff6a80f59a95e7317258b60
level1pr0grammer <157977041+level1pr0grammer@users.noreply.github.com>
2025-01-25 11:49:10 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       host: env('DATABASE_HOST', '35.198.199.2'), // Public IP of your Cloud SQL instance
       port: env.int('DATABASE_PORT', 5432),      // Port should be an integer
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'fiatjwin'),
+      password: env('DATABASE_PASSWORD', 'editthis'),
       database: env('DATABASE_NAME', 'postgresql'), // Use the actual database name, NOT the instance connection name
       ssl: {
         rejectUnauthorized: false,  // Typically set to false for Cloud SQL
