c6bc6912589d572d52fea98def40c1e20bf16d52
TM-view <thethirdman04@gmail.com>
2025-03-04 15:12:56 +0700

    trigger deploy

--- config/cron.js
+++ config/cron.js
@@ -1,13 +1,11 @@
 const cron = require('node-cron');
 
 module.exports = () => {
-  // ตั้งค่าให้ทำงานทุกๆ 10 นาที
   cron.schedule('*/10 * * * *', async () => {
     try {
       await strapi.service('api::market-place.custom-service').checkAndUpdateStatus();
     } catch (error) {
       console.error('Error in checkAndUpdateStatus:', error);
-      //มันerror อะไรสักอย่าง แต่โปรแกรมทำงานได้ปกติ
     }
   });
 };
